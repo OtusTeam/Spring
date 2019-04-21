784204f5324f6eda3bdcd0b88e62cb530ed743cb
saroff <tarasoff154@gmail.com>
2019-04-21 23:36:30 +0300

    Updated for junit5

--- docker-test-containers/src/test/java/ru/otus/docker/DockerApplicationTests.java
+++ docker-test-containers/src/test/java/ru/otus/docker/DockerApplicationTests.java
@@ -1,25 +1,28 @@
 package ru.otus.docker;
 
 import com.mongodb.MongoClient;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.HashMap;
 
-@RunWith(SpringRunner.class)
+@Testcontainers
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class DockerApplicationTests {
     private MongoTemplate mongoTemplate;
-    @Rule
+    @Container
     public GenericContainer mongo = new GenericContainer("mongo").withExposedPorts(27017);
     
-    @Before()
+    @BeforeEach
     public void before() throws Exception {
         mongoTemplate = new MongoTemplate(new MongoClient(mongo.getContainerIpAddress(), mongo.getFirstMappedPort()), "test");
     }
