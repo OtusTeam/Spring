3f660544164ab74796251f0a6833675fdd286c27
Evgeny Maksimenko <evmaksimenko@yandex-team.ru>
2019-12-03 10:16:58 +0300

    Move parseQuestionsArray method from utility class to QuestionRepositoryImpl.

--- 2019-11/spring-01/spring-01-homework/src/main/java/ru/otus/spring/repository/QuestionRepositoryImpl.java
+++ 2019-11/spring-01/spring-01-homework/src/main/java/ru/otus/spring/repository/QuestionRepositoryImpl.java
@@ -1,18 +1,47 @@
 package ru.otus.spring.repository;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.otus.spring.domain.Question;
 
 import static ru.otus.spring.utils.Common.getResourceReader;
-import static ru.otus.spring.utils.Common.parseQuestionsArray;
 import static ru.otus.spring.utils.Common.readCSV;
 
 public class QuestionRepositoryImpl implements QuestionRepository {
+    private static final Logger LOGGER = LoggerFactory.getLogger(QuestionRepositoryImpl.class);
+
     private final String questionFileName;
 
     public QuestionRepositoryImpl(String questionFileName) {
         this.questionFileName = questionFileName;
+    }
+
+    public List<Question> parseQuestionsArray(List<String[]> questionsArray) {
+        List<Question> questionList = new ArrayList<>();
+        for (String[] line : questionsArray) {
+            if (line.length < 3) {
+                continue;
+            }
+            try {
+                Question question = new Question(
+                        line[0],
+                        Arrays.stream(line)
+                                .skip(1)
+                                .limit(line.length - 2L)
+                                .collect(Collectors.toList()),
+                        Integer.parseInt(line[line.length - 1])
+                );
+                questionList.add(question);
+            } catch (NumberFormatException e) {
+                LOGGER.warn("Can't parse question {}", Arrays.toString(line));
+            }
+        }
+        return questionList;
     }
 
     @Override
--- 2019-11/spring-01/spring-01-homework/src/main/java/ru/otus/spring/utils/Common.java
+++ 2019-11/spring-01/spring-01-homework/src/main/java/ru/otus/spring/utils/Common.java
@@ -5,18 +5,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import com.opencsv.CSVReader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import ru.otus.spring.domain.Question;
 
 public class Common {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Common.class);
-
     private Common() {
     }
 
@@ -39,17 +32,5 @@ public class Common {
         } catch (IOException e) {
             throw new RuntimeException("Error reading csv file: ", e);
         }
-    }
-
-    public static List<Question> parseQuestionsArray(List<String[]> questionsArray) {
-        List<Question> questionList = new ArrayList<>();
-        for (String[] line : questionsArray) {
-            try {
-                questionList.add(new Question(line));
-            } catch (IllegalArgumentException e) {
-                LOGGER.warn("Can't parse question {}", Arrays.toString(line));
-            }
-        }
-        return questionList;
     }
 }
