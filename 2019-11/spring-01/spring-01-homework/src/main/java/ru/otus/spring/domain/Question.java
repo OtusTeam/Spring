658791353674653a6a1fce12575b618b0fa70690
Evgeny Maksimenko <evmaksimenko@yandex-team.ru>
2019-12-03 10:16:21 +0300

    Remove Question constructor with array of String arg.

--- 2019-11/spring-01/spring-01-homework/src/main/java/ru/otus/spring/domain/Question.java
+++ 2019-11/spring-01/spring-01-homework/src/main/java/ru/otus/spring/domain/Question.java
@@ -15,21 +15,4 @@ public class Question {
     private String questionText;
     private List<String> answers;
     private Integer correctAnswer;
-
-    public Question(String... args) {
-        if (args == null || args.length < 3) {
-            throw new IllegalArgumentException("Question array is null or need more answers");
-        }
-        try {
-            questionText = args[0];
-            answers = Arrays.stream(args)
-                    .skip(1)
-                    .limit(args.length - 2L)
-                    .collect(Collectors.toList());
-            correctAnswer = Integer.parseInt(args[args.length - 1]);
-        } catch (NumberFormatException e) {
-            // We can't read question
-            throw new IllegalArgumentException("Can't parse question array");
-        }
-    }
 }
