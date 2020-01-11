9c97c8b04efdec87c3e40d641f81d693bb95728e
Evgeny Maksimenko <evmaksimenko@yandex-team.ru>
2020-01-11 14:31:48 +0300

    Add nested static configuration for a test.

--- 2019-11/spring-05/spring-05-homework/src/test/java/ru/otus/spring/repository/QuestionRepositoryTest.java
+++ 2019-11/spring-05/spring-05-homework/src/test/java/ru/otus/spring/repository/QuestionRepositoryTest.java
@@ -1,22 +1,24 @@
-package ru.otus.spring.repository;
+package ru.otus.spring.repositorytest;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import ru.otus.spring.domain.Question;
+import ru.otus.spring.repository.QuestionRepository;
+import ru.otus.spring.repository.QuestionRepositoryImpl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
 public class QuestionRepositoryTest {
+    @Autowired
     private QuestionRepository questionRepository;
-
-    @BeforeEach
-    void setUp() {
-        questionRepository = new QuestionRepositoryImpl("");
-    }
 
     @Test
     @DisplayName("Question Repository QueryAll test")
@@ -29,6 +31,13 @@ public class QuestionRepositoryTest {
         assertThat(questionList.size()).isEqualTo(2);
         assertThat(questionList.get(0).getQuestionText()).isEqualTo("Q1");
         assertThat(questionList.get(1).getAnswers().size()).isEqualTo(3);
+    }
 
+    @Configuration
+    static class NestedConfiguration {
+        @Bean
+        QuestionRepository getQuestionRepository() {
+            return new QuestionRepositoryImpl("");
+        }
     }
 }
