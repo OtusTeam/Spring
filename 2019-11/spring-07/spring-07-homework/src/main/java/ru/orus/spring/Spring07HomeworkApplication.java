5c651d52b389e97582115791b807c0391d888a2d
Evgeny Maksimenko <evmaksimenko@yandex-team.ru>
2020-02-02 14:29:38 +0300

    Add main class.

---
+++ 2019-11/spring-07/spring-07-homework/src/main/java/ru/orus/spring/Spring07HomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.orus.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07HomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring07HomeworkApplication.class, args);
+    }
+
+}
