492a916453c6595127df353781e5a3c45b7b7fd4
Evgeny Maksimenko <evmaksimenko@yandex-team.ru>
2020-02-02 14:30:06 +0300

    Add domain classes.

---
+++ 2019-11/spring-07/spring-07-homework/src/main/java/ru/orus/spring/domain/Genre.java
@@ -0,0 +1,42 @@
+package ru.orus.spring.domain;
+
+public class Genre {
+    private Long id;
+    private String name;
+
+    public Genre() {
+    }
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public Genre(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("");
+        sb.append("id=").append(id);
+        sb.append(", '").append(name).append('\'');
+        return sb.toString();
+    }
+}
