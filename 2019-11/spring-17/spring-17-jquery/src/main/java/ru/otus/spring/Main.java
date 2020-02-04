7d164434a317f0124a15221f7c26f81ebc4f7a13
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2020-02-04 22:18:04 +0600

    2019-11 - 17

---
+++ 2019-11/spring-17/spring-17-jquery/src/main/java/ru/otus/spring/Main.java
@@ -0,0 +1,27 @@
+package ru.otus.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.otus.spring.domain.Person;
+import ru.otus.spring.repostory.PersonRepository;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+
+    @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
+    @Autowired
+    private PersonRepository repository;
+
+    @PostConstruct
+    public void init() {
+        repository.save(new Person("Pushkin"));
+        repository.save(new Person("Lermontov"));
+    }
+}
