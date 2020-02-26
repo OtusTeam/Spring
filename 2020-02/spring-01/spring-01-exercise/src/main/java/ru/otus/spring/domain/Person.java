fbf9b1995103f03b56b0aa0f5f0da1c9fc871699
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2020-02-26 21:13:05 +0600

    2020-02 - 01: final in fields

--- 2020-02/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/domain/Person.java
+++ 2020-02/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/domain/Person.java
@@ -2,8 +2,8 @@ package ru.otus.spring.domain;
 
 public class Person {
 
-    private String name;
-    private int age;
+    private final String name;
+    private final int age;
 
     public Person(String name, int age) {
         this.name = name;
