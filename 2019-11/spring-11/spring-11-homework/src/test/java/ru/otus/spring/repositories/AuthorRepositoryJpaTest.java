d0c7df6d3c5ea07923d7b1aaf6005a26d9f56cb9
Evgeny Maksimenko <evmaksimenko@yandex-team.ru>
2020-02-29 23:03:48 +0300

    Add repositories tests.

---
+++ 2019-11/spring-11/spring-11-homework/src/test/java/ru/otus/spring/repositories/AuthorRepositoryJpaTest.java
@@ -0,0 +1,79 @@
+package ru.otus.spring.repositories;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import ru.otus.spring.models.Author;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Репозиторий на основе Jpa для работы со авторами ")
+@DataJpaTest
+class AuthorRepositoryJpaTest {
+
+    private static final int EXPECTED_NUMBER_OF_AUTHORS = 3;
+    private static final long FIRST_AUTHOR_ID = 1;
+    private static final String FIRST_AUTHOR_NAME = "Пелевин";
+    private static final String UPDATED_AUTHOR_NAME = "Каганов";
+
+    @Autowired
+    private AuthorRepositoryJpa repositoryJpa;
+
+    @Autowired
+    private TestEntityManager em;
+
+    @DisplayName(" должен корректно сохранять всю информацию об авторе")
+    @Test
+    void save() {
+        var author = new Author(0, UPDATED_AUTHOR_NAME);
+        repositoryJpa.save(author);
+        assertThat(author.getId()).isGreaterThan(0);
+
+        var actualAuthor = em.find(Author.class, author.getId());
+        assertThat(actualAuthor).isNotNull().matches(s -> !s.getName().equals(""));
+    }
+
+    @DisplayName(" должен загружать информацию о нужном авторе по его id")
+    @Test
+    void findById() {
+        var actualAuthor = repositoryJpa.findById(FIRST_AUTHOR_ID);
+        var expectedAuthor = em.find(Author.class, FIRST_AUTHOR_ID);
+        assertThat(actualAuthor.isPresent()).isTrue();
+        assertThat(actualAuthor.get().getName()).isEqualTo(FIRST_AUTHOR_NAME);
+        assertThat(actualAuthor).isPresent().get()
+                .isEqualToComparingFieldByField(expectedAuthor);
+    }
+
+    @DisplayName(" должен загружать список всех второв с полной информацией о них")
+    @Test
+    void findAll() {
+        var authors = repositoryJpa.findAll();
+        assertThat(authors).isNotNull().hasSize(EXPECTED_NUMBER_OF_AUTHORS);
+    }
+
+    @DisplayName(" должен загружать информацию о нужном авторе по его имени")
+    @Test
+    void findByName() {
+        var firstAuthor = em.find(Author.class, FIRST_AUTHOR_ID);
+        List<Author> authors = repositoryJpa.findByName(FIRST_AUTHOR_NAME);
+        assertThat(authors).hasSize(1);
+        assertThat(authors).containsOnlyOnce(firstAuthor);
+    }
+
+    @DisplayName(" должен удалять заданного автора по его id")
+    @Test
+    void deleteById() {
+        var firstAuthor = em.find(Author.class, FIRST_AUTHOR_ID);
+        assertThat(firstAuthor).isNotNull();
+        em.detach(firstAuthor);
+
+        repositoryJpa.deleteById(FIRST_AUTHOR_ID);
+        var deletedAuthor = em.find(Author.class, FIRST_AUTHOR_ID);
+
+        assertThat(deletedAuthor).isNull();
+    }
+}
