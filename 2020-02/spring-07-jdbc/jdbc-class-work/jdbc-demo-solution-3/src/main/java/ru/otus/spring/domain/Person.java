91534946308c7f02f3ad4d16f36f28b403547763
stvort <stvort@mail.ru>
2020-03-25 14:18:47 +0400

    2020-02 07-jdbc lesson classwork has been added

---
+++ 2020-02/spring-07-jdbc/jdbc-class-work/jdbc-demo-solution-3/src/main/java/ru/otus/spring/domain/Person.java
@@ -0,0 +1,20 @@
+package ru.otus.spring.domain;
+
+public class Person {
+
+    private final long id;
+    private final String name;
+
+    public Person(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
