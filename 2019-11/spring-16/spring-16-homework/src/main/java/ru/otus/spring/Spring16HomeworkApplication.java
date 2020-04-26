697d184babb138e4a4d28cd9ec6d1a4ce1479540
Evgeny Maksimenko <evmaksimenko@yandex-team.ru>
2020-04-26 19:23:27 +0300

    Add main app class.

---
+++ 2019-11/spring-16/spring-16-homework/src/main/java/ru/otus/spring/Spring16HomeworkApplication.java
@@ -0,0 +1,11 @@
+package ru.otus.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring16HomeworkApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Spring16HomeworkApplication.class, args);
+	}
+}
