df02a9fd7d8aac1980a929a9fb470a8cc2efaaea
Evgeny Maksimenko <evmaksimenko@yandex-team.ru>
2020-06-16 16:53:19 +0300

    Exercise 02

--- 2020-05/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/Main.java
+++ 2020-05/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/Main.java
@@ -1,13 +1,14 @@
 package ru.otus.spring;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import ru.otus.spring.domain.Person;
 import ru.otus.spring.service.PersonService;
 
+@ComponentScan
 public class Main {
-
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = null;
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
 
         PersonService service = context.getBean(PersonService.class);
 
