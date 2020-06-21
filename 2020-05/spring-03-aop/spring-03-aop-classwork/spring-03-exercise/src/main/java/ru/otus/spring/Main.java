ecf79d3a0632ebc89b9c0f9464cf9db8d96be738
Evgeny Maksimenko <evmaksimenko@yandex-team.ru>
2020-06-21 12:57:18 +0300

    Spring-03 exercise.

--- 2020-05/spring-03-aop/spring-03-aop-classwork/spring-03-exercise/src/main/java/ru/otus/spring/Main.java
+++ 2020-05/spring-03-aop/spring-03-aop-classwork/spring-03-exercise/src/main/java/ru/otus/spring/Main.java
@@ -1,20 +1,27 @@
 package ru.otus.spring;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.*;
 import ru.otus.spring.domain.Person;
 import ru.otus.spring.service.PersonService;
 
-@Configuration
-@ComponentScan
+//@Configuration
+//@ComponentScan
+//@EnableAspectJAutoProxy
 public class Main {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(Main.class);
-
-        PersonService service = context.getBean(PersonService.class);
-
-        Person ivan = service.getByName("Ivan");
-        System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
+//        AnnotationConfigApplicationContext context =
+//                new AnnotationConfigApplicationContext(Main.class);
+//
+//        PersonService service = context.getBean(PersonService.class);
+//
+//        Person ivan = service.getByName("Ivan");
+//        System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
+        List<Object> collect = Optional.ofNullable(null).stream().collect(Collectors.toList());
+        System.out.println(collect.toString());
     }
 }
--- 2020-05/spring-03-aop/spring-03-aop-classwork/spring-03-exercise/src/main/java/ru/otus/spring/logging/LoggingAspect.java
+++ 2020-05/spring-03-aop/spring-03-aop-classwork/spring-03-exercise/src/main/java/ru/otus/spring/logging/LoggingAspect.java
@@ -1,13 +1,40 @@
 package ru.otus.spring.logging;
 
-import org.aspectj.lang.JoinPoint;
+import java.util.Arrays;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
 
+    @Before("execution(public ru.otus.spring.domain.Person ru.otus.spring.dao.PersonDaoSimple.findByName(String))")
     public void logBefore(JoinPoint joinPoint) {
         System.out.println("Прокси : " + joinPoint.getThis().getClass().getName());
         System.out.println("Класс : " + joinPoint.getTarget().getClass().getName());
 
         System.out.println("Вызов метода : " + joinPoint.getSignature().getName());
+    }
+
+    @Around("execution(* ru.otus.spring..*(..))")
+    public Object logAround(ProceedingJoinPoint point) {
+        if (point != null) {
+            System.out.println("Class: " + point.getTarget().toString());
+            System.out.println("Method: " + point.getSignature().getName());
+            System.out.println("Args: " + Arrays.toString(point.getArgs()));
+            try {
+                Object result = point.proceed();
+                System.out.println("Result: " + result != null ? result.toString() : "null");
+                return result;
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+                return null;
+            }
+        } else return null;
     }
 }
