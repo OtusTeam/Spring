9c3caa7fa0df1745a324e04882b7617c26a21f8c
Александр Оруджев <stvort@mail.ru>
2020-09-11 18:02:23 +0400

    2020-08 spring-05-adv-conf added

---
+++ 2020-08/spring-05-adv-conf/beans-scopes-exercise/src/main/java/ru/otus/example/beansscopesdemo/services/PrototypeGreetingServiceImpl.java
@@ -0,0 +1,7 @@
+package ru.otus.example.beansscopesdemo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service("PrototypeGreetingService")
+public class PrototypeGreetingServiceImpl extends AbstractGreetingServiceImpl {
+}
