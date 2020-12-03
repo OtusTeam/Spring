c0d1f0222d23f601a0fe7165cfcee126e2dd0c80
Sergey Petrelevich <petrelevich@yandex.ru>
2020-12-04 01:25:43 +0300

    L03 spring-boot

---
+++ 2020-11/spring-03-springboot/application/src/main/java/ru/otus/mainpackage/configs/LocalizationConfig.java
@@ -0,0 +1,17 @@
+package ru.otus.mainpackage.configs;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class LocalizationConfig {
+    @Bean
+    public MessageSource messageSource() {
+        var ms = new ReloadableResourceBundleMessageSource();
+        ms.setBasename("classpath:/i18n/bundle");
+        ms.setDefaultEncoding("UTF-8");
+        return ms;
+    }
+}
