cdff28199aae72cc226bdaf5332aafb5e205078c
Александр Оруджев <stvort@mail.ru>
2020-12-11 00:56:45 +0400

    2020-11 adv config examples added and testing examples updatetd

--- examples/unit-testing-spring-boot/src/test/java/ru/otus/testingExample/springboot/services/GreetingServiceImplTest.java
+++ examples/unit-testing-spring-boot/src/test/java/ru/otus/testingExample/springboot/services/GreetingServiceImplTest.java
@@ -3,7 +3,6 @@ package ru.otus.testingExample.springboot.services;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
