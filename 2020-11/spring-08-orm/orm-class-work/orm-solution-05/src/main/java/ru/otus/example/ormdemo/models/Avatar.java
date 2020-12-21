e5c553a5e952784465169851bd9dedd5fc1d8f5e
Александр Оруджев <stvort@mail.ru>
2020-12-21 23:45:04 +0400

    2020-11 spring-08-orm added

---
+++ 2020-11/spring-08-orm/orm-class-work/orm-solution-05/src/main/java/ru/otus/example/ormdemo/models/Avatar.java
@@ -0,0 +1,21 @@
+package ru.otus.example.ormdemo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "avatars")
+public class Avatar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "photo_url")
+    private String photoUrl;
+}
