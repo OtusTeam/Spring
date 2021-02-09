d5759a497ab7ca07ddfda1e98c90910d68cbf6f0
saroff <tarasoff154@gmail.com>
2021-02-09 19:06:06 +0300

    20-11 Spring 19 Reactive frameworks

--- 2020-11/spring-19/spring-19-reactive-spring-data/src/main/java/ru/otus/spring/Main.java
+++ 2020-11/spring-19/spring-19-reactive-spring-data/src/main/java/ru/otus/spring/Main.java
@@ -1,19 +1,8 @@
 package ru.otus.spring;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.context.ApplicationContext;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import reactor.core.Disposable;
-import reactor.core.publisher.Flux;
-import ru.otus.spring.domain.Account;
-import ru.otus.spring.domain.Person;
 import ru.otus.spring.repostory.AccountRepository;
 import ru.otus.spring.repostory.PersonRepository;
 
@@ -27,7 +16,5 @@ public class Main {
         AccountRepository accountRepository = context.getBean(AccountRepository.class);
 
         Thread.sleep(20000);
-
-        flux.dispose();
     }
 }
