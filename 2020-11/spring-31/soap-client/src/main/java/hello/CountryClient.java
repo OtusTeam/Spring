f44c9c90a46864e9f353d465a8f4126bc201573a
kataus <kataus@gmail.com>
2021-04-02 19:43:01 +0300

    spring-31 2020-11

---
+++ 2020-11/spring-31/soap-client/src/main/java/hello/CountryClient.java
@@ -0,0 +1,30 @@
+
+package hello;
+
+import hello.wsdl.GetCountryRequest;
+import hello.wsdl.GetCountryResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+public class CountryClient extends WebServiceGatewaySupport {
+
+    private static final Logger log = LoggerFactory.getLogger(CountryClient.class);
+
+    public GetCountryResponse getCountry(String country) {
+
+        GetCountryRequest request = new GetCountryRequest();
+        request.setName(country);
+
+        log.info("Requesting location for " + country);
+
+        GetCountryResponse response = (GetCountryResponse) getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:8080/ws/countries", request,
+                        new SoapActionCallback(
+                                "http://spring.io/guides/gs-producing-web-service/GetCountryRequest"));
+
+        return response;
+    }
+
+}
