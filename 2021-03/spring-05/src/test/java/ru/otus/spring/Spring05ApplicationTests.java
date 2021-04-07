d6fa0edaee8117bfc93b7adc11b20d0ef38e20b6
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2021-04-07 19:31:12 +0600

    Spring-2021-03 - 05 (Boot)

---
+++ 2021-03/spring-05/src/test/java/ru/otus/spring/Spring05ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
