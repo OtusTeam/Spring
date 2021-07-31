97dde15652751940ec8f7abf70f1da5ee3d34936
SmirnovaEU <62511446+SmirnovaEU@users.noreply.github.com>
2021-07-31 17:05:36 +0300

    spring shell + spring boot test

--- 2021-05/spring-05-adv-conf/homework4/src/test/java/ru/otus/spring/homework4/service/MessageServiceImplTest.java
+++ 2021-05/spring-05-adv-conf/homework4/src/test/java/ru/otus/spring/homework4/service/MessageServiceImplTest.java
@@ -1,12 +1,12 @@
 //package ru.otus.spring.homework4.service;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.MessageSource;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.SpringBootConfiguration;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.context.MessageSource;
 
 import java.util.Locale;
 
