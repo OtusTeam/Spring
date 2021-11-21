57da5c7cfb24d6e968c95ae7573a85b05eb224f9
SmirnovaEU <62511446+SmirnovaEU@users.noreply.github.com>
2021-11-21 18:52:51 +0300

    v2

--- 2021-05/homework8_mongo/src/main/java/com/otus/homework/homework8_mongo/service/BookServiceImpl.java
+++ 2021-05/homework8_mongo/src/main/java/com/otus/homework/homework8_mongo/service/BookServiceImpl.java
@@ -5,7 +5,6 @@ import com.otus.homework.homework8_mongo.domain.Book;
 import com.otus.homework.homework8_mongo.repository.AuthorRepository;
 import com.otus.homework.homework8_mongo.repository.BookRepository;
 import com.otus.homework.homework8_mongo.repository.GenreRepository;
-import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -49,52 +48,7 @@ public class BookServiceImpl implements BookService{
         repository.deleteById(bookId);
 
     }
-
-//    @Override
-//    public Book saveWithConstraints(Book book) {
-//        repository.save(book);
-//        Author author = book.getAuthor();
-//        List<Book> books = author.getBooks() == null ? new ArrayList<>(): author.getBooks();
-//        books.add(book);
-//        author.setBooks(books);
-//        authorRepository.save(author);
-//        Genre genre = book.getGenre();
-//        books = genre.getBooks()== null ? new ArrayList<>():genre.getBooks();
-//        books.add(book);
-//        genre.setBooks(books);
-//        genreRepository.save(genre);
-//        repository.saveWithConstraints(book);
-//        return book;
-//    }
-
-//    @Override
-//    public Book updateTitleById(String bookId, String title) throws NotFoundException {
-//        Book book = repository.findById(bookId).orElseThrow(() -> new NotFoundException("There is no book with id = "+ bookId));
-//        book.setTitle(title);
-//        //обновить книгу в списке автора
-//        Author author = book.getAuthor();
-//        List<Book> books = author.getBooks() == null ? new ArrayList<>(): author.getBooks();
-//        books.remove(bookId);
-//        books.add(book);
-//        author.setBooks(books);
-//        authorRepository.save(author);
-//        //обновить книгу в списке жанра
-//        Genre genre = book.getGenre();
-//        books = genre.getBooks()== null ? new ArrayList<>():genre.getBooks();
-//        books.remove(bookId);
-//        books.add(book);
-//        genre.setBooks(books);
-//        genreRepository.save(genre);
-//        repository.saveWithConstraints(book);
-//        return book;
-//    }
-
-//    @Override
-//    public long countAttachedComments(String bookId) throws NotFoundException {
-//        Book book = repository.findById(bookId).orElseThrow(() -> new NotFoundException("There is no book with id = "+ bookId));
-//        return book.getComments().size();
-//    }
-
+    
     @Override
     public void removeBookWithComments(String bookId) {
         repository.removeBookWithComments(bookId);
