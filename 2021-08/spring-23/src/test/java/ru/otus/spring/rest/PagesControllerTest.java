69ffd9b0081a45c951863e7a5b6cf945512d6acb
kataus <kataus@gmail.com>
2021-11-27 11:04:13 +0300

    примеры к занятию по аутентификации Spring

--- 2021-08/spring-23/src/test/java/ru/otus/spring/rest/PagesControllerTest.java
+++ 2021-08/spring-23/src/test/java/ru/otus/spring/rest/PagesControllerTest.java
@@ -1,17 +1,19 @@
 package ru.otus.spring.rest;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(PagesController.class)
 public class PagesControllerTest {
 
