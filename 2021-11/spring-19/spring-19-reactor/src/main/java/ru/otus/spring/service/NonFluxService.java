42672ecb46c8dd84b03a384c918c7d0d73a07d48
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2022-02-05 18:18:37 +0300

    A bit of reactor updates

--- 2021-11/spring-19/spring-19-reactor/src/main/java/ru/otus/spring/service/NonFluxService.java
+++ 2021-11/spring-19/spring-19-reactor/src/main/java/ru/otus/spring/service/NonFluxService.java
@@ -13,8 +13,8 @@ public class NonFluxService {
     public Message nonFluxSayHello(Message message) {
         logger.info("Message received in non-flux service: {}", message.getValue());
 
-        final String name = message.getValue();
-        final String withHello = "Hello, " + name + "!";
+        String name = message.getValue();
+        String withHello = "Hello, " + name + "!";
         try {
             Thread.sleep(1000);
             return new Message(withHello);
