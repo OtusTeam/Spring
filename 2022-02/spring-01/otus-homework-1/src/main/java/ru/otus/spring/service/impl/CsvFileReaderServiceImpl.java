17046e4e6d7be5327f9d9083eeb3125261edb720
kirill <mrkami19940@gmail.com>
2022-03-09 19:17:03 +0300

    done homework 2

--- 2022-02/spring-01/otus-homework-1/src/main/java/ru/otus/spring/service/impl/CsvFileReaderServiceImpl.java
+++ 2022-02/spring-01/otus-homework-1/src/main/java/ru/otus/spring/service/impl/CsvFileReaderServiceImpl.java
@@ -2,17 +2,20 @@ package ru.otus.spring.service.impl;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.otus.spring.dao.CsvFileReaderDao;
 import ru.otus.spring.domain.Task;
 import ru.otus.spring.service.CsvFileReaderService;
 
 import java.util.List;
 
+@Service
 @Data
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class CsvFileReaderServiceImpl implements CsvFileReaderService {
 
-    private CsvFileReaderDao dao;
+    private final CsvFileReaderDao dao;
 
     @Override
     public List<Task> getTaskList() {
--- 2022-02/spring-01/otus-homework-1/src/test/java/CsvFileReaderTest.java
+++ 2022-02/spring-01/otus-homework-1/src/test/java/CsvFileReaderTest.java
@@ -5,6 +5,8 @@ import ru.otus.spring.dao.CsvFileReaderDaoSimple;
 import ru.otus.spring.domain.Task;
 
 import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -12,14 +14,28 @@ import static org.junit.Assert.assertTrue;
 
 public class CsvFileReaderTest {
 
-    private static final String CSV_PATH = "/home/kirill/IdeaProjects/otus/homework/otus-homework/src/main/resources/tasks.csv";
+    private static final String CSV_PATH = "/tasks.csv";
 
     private static final String CONTEXT = "/spring-context.xml";
 
     @Test
     public void getTaskListTest() throws Exception {
-        CSVReader csvReader = new CSVReader(new FileReader(CSV_PATH));
-        List<Task> taskList = CsvFileReaderDaoSimple.getTaskList(csvReader.readAll());
+        CSVReader csvReader = new CSVReader(new InputStreamReader(this.getClass().getResourceAsStream(CSV_PATH)));
+        List<String[]> list = csvReader.readAll();
+        List<Task> taskList = new ArrayList<>();
+        if (!list.isEmpty()) {
+            list.forEach(l -> {
+                String[] str = l[0].replaceAll("\"", "").trim().split("\\;");
+                if (str.length == 3) {
+                    String[] responses = str[1].trim().split("\\,");
+                    if (responses.length > 0) {
+                        List<String> responseList = List.of(responses);
+                        taskList.add(new Task(str[0], responseList, str[2]));
+                    }
+                }
+            });
+        }
+
         assertEquals(taskList.size(), 4);
     }
 
