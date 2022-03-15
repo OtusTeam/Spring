93c4a0f69ff3497e2395070e9988e933e0c91aab
kirill <mrkami19940@gmail.com>
2022-03-15 16:57:25 +0300

    done homework 3 (spring-boot)

---
+++ 2022-02/spring-01/spring-boot-homework/src/main/java/ru/otus/spring/dao/InputStreamReaderDaoSimple.java
@@ -0,0 +1,30 @@
+package ru.otus.spring.dao;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+@Getter
+@Service
+public class InputStreamReaderDaoSimple implements InputStreamReaderDao {
+
+    @Value("${resource.csv.file}")
+    private String csvFilePath;
+
+    private InputStreamReader reader;
+
+    @Override
+    public void initReader() {
+        this.reader = new InputStreamReader(Objects.requireNonNull(this.getClass().getResourceAsStream(csvFilePath)));
+    }
+
+    @Override
+    public void closeReader() throws IOException {
+        this.reader.close();
+    }
+}
