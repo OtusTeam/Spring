a02bfa26290141cd1f869b71f072801351200343
stvort <stvort@mail.ru>
2022-03-17 02:47:40 +0400

    2021-11 spring-26-spring-batch added

---
+++ 2021-11/spring-26-spring-batch/src/main/java/ru/otus/example/springbatch/config/BatchConfig.java
@@ -0,0 +1,19 @@
+package ru.otus.example.springbatch.config;
+
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig {
+    @Bean
+    public JobRegistryBeanPostProcessor postProcessor(JobRegistry jobRegistry) {
+        var processor = new JobRegistryBeanPostProcessor();
+        processor.setJobRegistry(jobRegistry);
+        return processor;
+    }
+
+}
