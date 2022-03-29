a463a9eb9edc30673898adad03dd43f4b3a5e905
kataus <kataus@gmail.com>
2022-03-29 20:01:41 +0300

    Примеры 29 2021-11

---
+++ 2021-11/spring-29/spring-29-solution/src/main/java/ru/otus/spring/integration/domain/Food.java
@@ -0,0 +1,15 @@
+package ru.otus.spring.integration.domain;
+
+
+public class Food {
+
+    private final String name;
+
+    public Food(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
