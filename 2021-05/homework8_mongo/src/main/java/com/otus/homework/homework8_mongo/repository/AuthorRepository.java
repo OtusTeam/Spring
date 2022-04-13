6414865cb34c9b9a68b879bf4b7d31129458e60d
Elena <smirnova.eu@gmail.com>
2022-04-13 22:40:30 +0300

    v.2

--- 2021-05/homework8_mongo/src/main/java/com/otus/homework/homework8_mongo/repository/AuthorRepository.java
+++ 2021-05/homework8_mongo/src/main/java/com/otus/homework/homework8_mongo/repository/AuthorRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface AuthorRepository extends MongoRepository<Author, String>, AuthorRepositoryCustom {
+public interface  AuthorRepository extends MongoRepository<Author, String>, AuthorRepositoryCustom {
     Author getById(String id);
 
     Optional<Author> findByName(String name);
