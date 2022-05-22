4cd00d3ad9259b0308a2aef1398b0b380960382e
kirill <mrkami19940@gmail.com>
2022-05-22 18:40:48 +0300

    galeevki: done data jpa homework (v2 - drop unusable annotation)

--- 2022-02/spring-11-data-jpa/data-jpa-homework/src/main/java/ru/otus/spring/service/LibraryService.java
+++ 2022-02/spring-11-data-jpa/data-jpa-homework/src/main/java/ru/otus/spring/service/LibraryService.java
@@ -7,27 +7,19 @@ import java.util.List;
 
 public interface LibraryService {
 
-    @Transactional(readOnly = true)
     List<BookDto> getAllBooks();
 
-    @Transactional(readOnly = true)
     List<String> getAllBookName();
 
-    @Transactional(readOnly = true)
     BookDto getBookById(long id);
 
-    @Transactional(readOnly = true)
     BookDto getBookByName(String name);
 
-    @Transactional
     BookDto addNewBook(String bookName, String authorName, String genreName, String commentText);
 
-    @Transactional
     void updateBookNameById(long id, String name);
 
-    @Transactional
     void deleteBookById(long id);
 
-    @Transactional
     void deleteBookByName(String name);
 }
