15cdbfd575e8a942f73b7207b32eb1f7c104f15d
kirill <mrkami19940@gmail.com>
2022-05-22 17:11:47 +0300

    galeevki: done data jpa homework (v1)

---
+++ 2022-02/spring-11-data-jpa/data-jpa-homework/src/main/java/ru/otus/spring/repositories/BookRepository.java
@@ -0,0 +1,19 @@
+package ru.otus.spring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.otus.spring.models.Book;
+
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findByName(String name);
+
+    @Modifying
+    @Query(value = "DELETE FROM Book b " +
+            "WHERE b.name = :name")
+    void deleteByName(@Param("name") String name);
+}
