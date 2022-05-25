27a5fc1157865a975dab1c2dcb8bd89cc2c10321
kataus <kataus@gmail.com>
2022-05-25 19:44:20 +0300

    примеры по spring-security для 2022-02

--- 2022-02/spring-24/src/main/java/ru/otus/spring/security/SecurityConfiguration.java
+++ 2022-02/spring-24/src/main/java/ru/otus/spring/security/SecurityConfiguration.java
@@ -2,10 +2,7 @@ package ru.otus.spring.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.intercept.aopalliance.MethodSecurityInterceptor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,62 +12,67 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.util.Collection;
 
 @EnableWebSecurity
-@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/");
+        web.ignoring()
+                .antMatchers("/")
+                .antMatchers( "/static/**" );
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                //.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                //.and()
-                .authorizeRequests().antMatchers("/public").permitAll()
+                // По умолчанию SecurityContext хранится в сессии. Эта часть вырубает и каждый запросом приходитТ
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+//                .and()
+                .authorizeRequests()
+                .antMatchers("/public/").anonymous()
                 .and()
-                .authorizeRequests().antMatchers("/authenticated", "/success").authenticated()
-                .and()
-                .authorizeRequests().antMatchers("/user").hasAnyRole( "ADMIN", "USER" )
-
-                .and()
-                .authorizeRequests().antMatchers("/admin").hasRole( "ADMIN" )
+                .authorizeRequests()
+                .antMatchers("/authenticated").authenticated()
+//                .and()
+//                .authorizeRequests().antMatchers("/public").authenticated()
                 .and()
                 .formLogin()
                 .and()
-                .logout().logoutUrl("/logout");
+                .anonymous()
+                .principal( "anonymous" )
+                .and()
+                .rememberMe().key( "Some secret" )
+        ;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence charSequence) {
-                return charSequence.toString();
-            }
-
-            @Override
-            public boolean matches(CharSequence charSequence, String s) {
-                return charSequence.toString().equals(s);
-            }
-        };
+//        return new BCryptPasswordEncoder(10);
+        return NoOpPasswordEncoder.getInstance();
+//        return new PasswordEncoder() {
+//            @Override
+//            public String encode(CharSequence charSequence) {
+//                return charSequence.toString();
+//            }
+//
+//            @Override
+//            public boolean matches(CharSequence charSequence, String s) {
+//                return charSequence.toString().equals(s);
+//            }
+//        };
     }
 
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("admin").password("password").roles("ADMIN")
-                .and()
-                .withUser("user").password("password").roles("USER")
-                .and()
-                .withUser("manager").password("manager").roles("MANAGER", "USER");
+                ;
     }
 }
