573f241f47beff9bf0a12a860a00e490607453ae
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2022-06-02 00:52:13 +0600

    How in my project

--- 2022-02/spring-26/src/main/java/ru/otus/spring/SpringSecurityAuthorization.java
+++ 2022-02/spring-26/src/main/java/ru/otus/spring/SpringSecurityAuthorization.java
@@ -2,11 +2,7 @@ package ru.otus.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @SpringBootApplication
 public class SpringSecurityAuthorization {
 
