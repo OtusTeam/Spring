c860c073162f17f744d5768d83e39323a30a9c15
kirill <mrkami19940@gmail.com>
2022-06-14 14:02:19 +0300

    galeevki: done security homework (v1)

---
+++ homeworks/spring-security-homework/src/main/java/ru/otus/spring/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,20 @@
+package ru.otus.spring.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.otus.spring.repositories.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+}
