07020e76a38b774271053c2ef91f2427b2c3c05a
ydvorzhetskiy <dvorzhetskij@mail.ru>
2022-08-09 23:22:52 +0600

    Fix

--- 2022-05/spring-21/spring-21-web-flux/src/main/java/ru/otus/spring/ReactorController.java
+++ 2022-05/spring-21/spring-21-web-flux/src/main/java/ru/otus/spring/ReactorController.java
@@ -1,5 +1,6 @@
 package ru.otus.spring;
 
+import io.reactivex.Flowable;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,16 +19,15 @@ public class ReactorController {
 
     @GetMapping("/flux/ten")
     public Flux<Integer> list() {
-        return repository.findAll()
-            .map();
+        return Flux.range(1, 10);
     }
 
     @GetMapping(path = "/flux/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> stream() {
         return Flux.generate(() -> 0, (state, emitter) -> {
-            emitter.next(state);
-            return state + 1;
-        })
+                    emitter.next(state);
+                    return state + 1;
+                })
                 .delayElements(Duration.ofSeconds(1L))
                 .map(Object::toString);
     }
