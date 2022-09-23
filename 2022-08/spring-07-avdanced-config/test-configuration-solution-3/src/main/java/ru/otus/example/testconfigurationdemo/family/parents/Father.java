5f1cc6f52109353893440a253006672c311fa284
stvort <stvort@mail.ru>
2022-09-24 02:03:47 +0400

    2022-08 spring-07-avdanced-config added

---
+++ 2022-08/spring-07-avdanced-config/test-configuration-solution-3/src/main/java/ru/otus/example/testconfigurationdemo/family/parents/Father.java
@@ -0,0 +1,10 @@
+package ru.otus.example.testconfigurationdemo.family.parents;
+
+import ru.otus.example.testconfigurationdemo.family.FamilyMember;
+
+public class Father extends FamilyMember {
+    @Override
+    public String getName() {
+        return "Папа";
+    }
+}
