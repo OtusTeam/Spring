49b8630b6bc24429139986e7f9ee75dc2556b20b
stvort <stvort@mail.ru>
2022-11-09 00:30:06 +0400

    2022-08 spring-18-view small fix

--- 2022-08/spring-18-view/spring-mvc-view-demo/src/main/java/ru/otus/spring/controller/PersonController.java
+++ 2022-08/spring-18-view/spring-mvc-view-demo/src/main/java/ru/otus/spring/controller/PersonController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,7 +35,6 @@ public class PersonController {
         return "edit";
     }
 
-    @Validated
     @PostMapping("/edit")
     public String savePerson(@Valid @ModelAttribute("person") PersonDto person,
                              BindingResult bindingResult, Model model) {
