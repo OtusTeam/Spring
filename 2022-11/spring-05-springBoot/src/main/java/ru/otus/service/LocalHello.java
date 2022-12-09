cefdab657d3674aab7f32e6541b7ae2d444257e2
sergey <petrelevich@yandex.ru>
2022-12-09 22:01:15 +0300

    L05 SpringBoot

--- 2022-11/spring-05-springBoot/src/main/java/ru/otus/service/LocalHello.java
+++ 2022-11/spring-05-springBoot/src/main/java/ru/otus/service/LocalHello.java
@@ -1,6 +1,6 @@
 package ru.otus.service;
 
-import jakarta.annotation.PostConstruct;
+import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
