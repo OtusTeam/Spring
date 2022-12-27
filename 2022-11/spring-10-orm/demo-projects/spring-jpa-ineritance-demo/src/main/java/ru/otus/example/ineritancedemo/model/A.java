e1e60bb416db809ef2e2aeb0f1029cd7326c6221
stvort <stvort@mail.ru>
2022-12-27 11:46:14 +0400

    2022-11 spring-10-orm added

---
+++ 2022-11/spring-10-orm/demo-projects/spring-jpa-ineritance-demo/src/main/java/ru/otus/example/ineritancedemo/model/A.java
@@ -0,0 +1,37 @@
+package ru.otus.example.ineritancedemo.model;
+
+import javax.persistence.*;
+
+@Entity
+@DiscriminatorColumn(name = "discriminator")
+@DiscriminatorValue("RootA")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//@Inheritance(strategy = InheritanceType.JOINED)
+//@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Table(name = "A")
+public class A {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    // Не получится использовать при InheritanceType.TABLE_PER_CLASS
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected long id;
+
+    protected String a;
+
+    public A() {
+    }
+
+    public A(long id, String a) {
+        this.id = id;
+        this.a = a;
+    }
+
+    @Override
+    public String toString() {
+        return "A{" +
+                "id=" + id +
+                ", a='" + a + '\'' +
+                '}';
+    }
+}
