58d00b50b0861d34dbec8aedf4c1b9054f13965f
DiK <realcorwin@mail.ru>
2022-12-28 15:32:30 +0100

    2022-08\spring-actuator-and-data-rest

---
+++ 2022-08/spring-32/spring-32-exercise/src/main/java/ru/otus/spring/microservice/domain/Person.java
@@ -0,0 +1,37 @@
+package ru.otus.spring.microservice.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
