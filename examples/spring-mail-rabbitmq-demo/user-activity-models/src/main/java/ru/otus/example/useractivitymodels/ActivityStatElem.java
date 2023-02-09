c8aab957141c4b63cf22aa929b2a913357480f82
stvort <stvort@mail.ru>
2023-02-09 22:35:10 +0400

    example edited

--- examples/spring-mail-rabbitmq-demo/user-activity-models/src/main/java/ru/otus/example/useractivitymodels/ActivityStatElem.java
+++ examples/spring-mail-rabbitmq-demo/user-activity-models/src/main/java/ru/otus/example/useractivitymodels/ActivityStatElem.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Data
 @NoArgsConstructor
