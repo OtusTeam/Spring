cfab92500477c6ee9689ebcb6b778a5a567a1004
stvort <stvort@mail.ru>
2023-02-15 01:26:53 +0400

    2023-01 spring-06-bean-scopes-and-lifecycle added

---
+++ 2023-01/spring-06-bean-scopes-and-lifecycle/beans-scopes-exercise/src/main/java/ru/otus/example/beansscopesdemo/services/SessionGreetingServiceImpl.java
@@ -0,0 +1,7 @@
+package ru.otus.example.beansscopesdemo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service("SessionGreetingService")
+public class SessionGreetingServiceImpl extends AbstractGreetingServiceImpl {
+}
