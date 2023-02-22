1f1da4db4c162733be583d4a84d9cfc3c3beec47
stvort <stvort@mail.ru>
2023-02-22 20:17:14 +0400

    examples dependencies updated

--- examples/unit-testing-pure-spring-parallel/src/test/java/ru/otus/testing/example/services/OpenedIOServiceTest.java
+++ examples/unit-testing-pure-spring-parallel/src/test/java/ru/otus/testing/example/services/OpenedIOServiceTest.java
@@ -1,6 +1,5 @@
 package ru.otus.testing.example.services;
 
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,19 +30,17 @@ class OpenedIOServiceTest {
     }
 
     @DisplayName("должно печатать \"" + TEXT_TO_PRINT1 + "\"")
-    @SneakyThrows
     @Test
-    void shouldPrintOnlyFirstCreedLine() {
+    void shouldPrintOnlyFirstCreedLine() throws InterruptedException {
         ioService.out(TEXT_TO_PRINT1);
         Thread.sleep(1000);
-        assertThat(bos.toString()).isEqualTo(TEXT_TO_PRINT1 + "\r\n");
+        assertThat(bos.toString()).isEqualTo(TEXT_TO_PRINT1 + System.lineSeparator());
     }
 
     @DisplayName("должно печатать \"" + TEXT_TO_PRINT2 + "\"")
-    @SneakyThrows
     @Test
     void shouldPrintOnlySecondCreedLine() {
         ioService.out(TEXT_TO_PRINT2);
-        assertThat(bos.toString()).isEqualTo(TEXT_TO_PRINT2 + "\r\n");
+        assertThat(bos.toString()).isEqualTo(TEXT_TO_PRINT2 + System.lineSeparator());
     }
 }
