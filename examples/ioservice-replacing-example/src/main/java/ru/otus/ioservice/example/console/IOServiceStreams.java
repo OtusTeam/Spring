e5bbbbdcd675004de1f07e21271b29ba32062362
stvort <stvort@mail.ru>
2023-02-22 21:57:24 +0400

    examples dependencies updated

--- examples/ioservice-replacing-example/src/main/java/ru/otus/ioservice/example/console/IOServiceStreams.java
+++ examples/ioservice-replacing-example/src/main/java/ru/otus/ioservice/example/console/IOServiceStreams.java
@@ -11,12 +11,12 @@ import java.util.Scanner;
 
 @ConditionalOnProperty(name = "use.console", havingValue = "true")
 @Service
-public class ConsoleIOService implements IOService {
+public class IOServiceStreams implements IOService {
     private final PrintStream out;
     private final Scanner sc;
 
 
-    public ConsoleIOService(@Value("#{T(java.lang.System).out}") PrintStream out,
+    public IOServiceStreams(@Value("#{T(java.lang.System).out}") PrintStream out,
                             @Value("#{T(java.lang.System).in}") InputStream in) {
         this.out = out;
         this.sc = new Scanner(in);
