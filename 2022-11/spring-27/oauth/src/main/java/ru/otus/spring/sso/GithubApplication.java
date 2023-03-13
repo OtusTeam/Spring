a50b20e2a15cf5880f064795206e23df6dd7130d
kataus <kataus@gmail.com>
2023-03-13 17:56:41 +0300

    подготовка примеров к занятию 27 Spring 22-11

---
+++ 2022-11/spring-27/oauth/src/main/java/ru/otus/spring/sso/GithubApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.spring.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run( GithubApplication.class, args);
+	}
+
+}
