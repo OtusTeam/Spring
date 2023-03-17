b39c2ac4dcd2c5e63d20ac43225508c1d55b8a73
stvort <stvort@mail.ru>
2023-03-17 20:07:58 +0400

    2022-11 spring-28-spring- small fix

--- 2022-11/spring-28-spring-batch/src/main/java/ru/otus/example/springbatch/Main.java
+++ 2022-11/spring-28-spring-batch/src/main/java/ru/otus/example/springbatch/Main.java
@@ -3,9 +3,6 @@ package ru.otus.example.springbatch;
 import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-
-import java.io.IOException;
 
 @EnableMongock
 @SpringBootApplication
