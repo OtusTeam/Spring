49a8bdc654f18140d8401327b2b01b3047b464e4
stvort <stvort@mail.ru>
2023-03-26 13:06:15 +0400

    2023-01 sprong-13-data-jpa boot and java version updated

--- 2023-01/spring-13-data-jpa/exercise/src/main/java/ru/otus/springdata/domain/Person.java
+++ 2023-01/spring-13-data-jpa/exercise/src/main/java/ru/otus/springdata/domain/Person.java
@@ -1,13 +1,12 @@
 package ru.otus.springdata.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Data
 @NoArgsConstructor
--- 2023-01/spring-13-data-jpa/solution-01/src/main/java/ru/otus/springdata/domain/Person.java
+++ 2023-01/spring-13-data-jpa/solution-01/src/main/java/ru/otus/springdata/domain/Person.java
@@ -1,10 +1,12 @@
 package ru.otus.springdata.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
