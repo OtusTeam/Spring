b4b83f45d4b923aa106abadf16766ba3d7741c20
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2023-03-28 11:02:08 +0300

    2022-11 spring-31 SI

---
+++ 2022-11/spring-31/spring-31-exercise/src/main/java/ru/otus/spring/integration/services/OrderServiceImpl.java
@@ -0,0 +1,45 @@
+package ru.otus.spring.integration.services;
+
+import org.apache.commons.lang3.RandomUtils;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+import ru.otus.spring.integration.domain.Food;
+import ru.otus.spring.integration.domain.OrderItem;
+
+@Service
+@Slf4j
+public class OrderServiceImpl implements OrderService {
+    private static final String[] MENU = {"coffee", "tea", "smoothie", "whiskey", "beer", "cola", "water"};
+
+    private final CafeGateway cafe;
+
+    public OrderServiceImpl(CafeGateway cafe) {
+        this.cafe = cafe;
+    }
+
+    @Override
+    public void startGenerateOrdersLoop() {
+        for (int i = 0; i < 10; i++) {
+            delay();
+
+            OrderItem orderItem = generateOrderItem();
+            int num = i + 1;
+            log.info("{}, New orderItem: {}", num, orderItem.getItemName());
+            Food food = cafe.process(orderItem);
+            log.info("{}, Ready food: {}", num, food.getName());
+        }
+    }
+
+    private static OrderItem generateOrderItem() {
+        return new OrderItem(MENU[RandomUtils.nextInt(0, MENU.length)]);
+    }
+
+    private void delay() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
