5a1fd5230bf5f3a8745c3efb968f913f875bfbe2
stvort <stvort@mail.ru>
2023-04-17 01:59:02 +0400

    2023-03 spring-07-advanced-config added

---
+++ 2023-03/spring-07-advanced-config/application-events-demo/src/main/java/ru/otus/example/applicationeventsdemo/ApplicationEventsDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.example.applicationeventsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationEventsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationEventsDemoApplication.class, args);
+	}
+
+}
