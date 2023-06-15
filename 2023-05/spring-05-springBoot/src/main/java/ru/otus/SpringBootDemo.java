ae61ab50031a5e0d4ec380256b78e1a707c66978
petrelevich <petrelevich@yandex.ru>
2023-06-15 21:51:33 +0300

    L05 springBoot

---
+++ 2023-05/spring-05-springBoot/src/main/java/ru/otus/SpringBootDemo.java
@@ -0,0 +1,12 @@
+package ru.otus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemo.class, args);
+    }
+}
