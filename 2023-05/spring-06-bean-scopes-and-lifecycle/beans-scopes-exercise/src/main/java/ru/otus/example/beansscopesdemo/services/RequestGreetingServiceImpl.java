d3f5e43fdea9001c1eab71c28e717d8d7f10fe23
stvort <stvort@mail.ru>
2023-06-20 19:31:15 +0400

    2023-05 spring-06-bean-scopes-and-lifecycle added

---
+++ 2023-05/spring-06-bean-scopes-and-lifecycle/beans-scopes-exercise/src/main/java/ru/otus/example/beansscopesdemo/services/RequestGreetingServiceImpl.java
@@ -0,0 +1,7 @@
+package ru.otus.example.beansscopesdemo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service("requestGreetingService")
+public class RequestGreetingServiceImpl extends AbstractGreetingServiceImpl {
+}
