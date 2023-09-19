2934371ed13d7fb3e844ae9f7684b46be2b7f087
stvort <stvort@mail.ru>
2023-09-19 22:58:41 +0400

    test container example updated

--- examples/docker-test-containers/src/test/java/ru/otus/docker/DockerApplicationTests.java
+++ examples/docker-test-containers/src/test/java/ru/otus/docker/DockerApplicationTests.java
@@ -1,36 +1,41 @@
 package ru.otus.docker;
 
-import com.mongodb.MongoClient;
-
-import org.junit.jupiter.api.BeforeEach;
+import org.bson.Document;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-import java.util.HashMap;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class DockerApplicationTests {
-    private MongoTemplate mongoTemplate;
-    @Container
-    public GenericContainer mongo = new GenericContainer("mongo").withExposedPorts(27017);
-    
-    @BeforeEach
-    public void before() throws Exception {
-        mongoTemplate = new MongoTemplate(new MongoClient(mongo.getContainerIpAddress(), mongo.getFirstMappedPort()), "test");
+    private static final int MONGO_PORT = 27017;
+    private static final String SOME_COLLECTION_NAME = "someCollection";
+    private static final String IMAGE_NAME = "mongo";
+
+    @SuppressWarnings("resource")
+    public static GenericContainer<?> mongo = new GenericContainer<>(DockerImageName.parse(IMAGE_NAME))
+            .withExposedPorts(MONGO_PORT);
+
+    @DynamicPropertySource
+    static void mongoProperties(DynamicPropertyRegistry registry) {
+        mongo.start();
+        registry.add("spring.data.mongodb.port", mongo::getFirstMappedPort);
+        registry.add("spring.data.mongodb.host", mongo::getHost);
     }
-    
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     @Test
-    public void contextLoads() {
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("name", "Steve");
-        mongoTemplate.save(map, "someCollection");
+    public void shouldSaveAndLoadDocument() {
+        Document document = Document.parse("{name: \"Steve\"}");
+        Document savedDocument = mongoTemplate.save(document, SOME_COLLECTION_NAME);
+        assertThat(savedDocument.get("_id")).isNotNull();
     }
 }
