205fdddda1865624da3ad1d4a52b756605c8a463
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2023-10-03 15:46:07 +0300

    2023-05 spring-32-http-client

---
+++ 2023-05/spring-32-http-client/soap-client/src/main/java/hello/ClientApp.java
@@ -0,0 +1,30 @@
+package hello;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hello.wsdl.GetCountryResponse;
+
+@SpringBootApplication
+public class ClientApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApp.class, args);
+	}
+
+	@Bean
+	CommandLineRunner lookup(CountryClient quoteClient) {
+		return args -> {
+			String country = "Spain";
+
+			if (args.length > 0) {
+				country = args[0];
+			}
+			GetCountryResponse response = quoteClient.getCountry(country);
+			System.err.println(response.getCountry().getCurrency());
+		};
+	}
+
+}
