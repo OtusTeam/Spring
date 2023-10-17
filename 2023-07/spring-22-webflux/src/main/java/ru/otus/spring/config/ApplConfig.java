82c6e1fd9ea556db9c5e53a291d0dcfdc5aed5d7
petrelevich <petrelevich@yandex.ru>
2023-10-17 22:30:46 +0300

    22 webflux

---
+++ 2023-07/spring-22-webflux/src/main/java/ru/otus/spring/config/ApplConfig.java
@@ -0,0 +1,39 @@
+package ru.otus.spring.config;
+
+import io.netty.channel.nio.NioEventLoopGroup;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+import reactor.util.annotation.NonNull;
+
+@Configuration
+public class ApplConfig {
+    private static final int THREAD_POOL_SIZE = 2;
+
+    @Bean
+    public ReactiveWebServerFactory reactiveWebServerFactory() {
+        var eventLoopGroup = new NioEventLoopGroup(THREAD_POOL_SIZE,
+                new ThreadFactory() {
+                    private final AtomicLong threadIdGenerator = new AtomicLong(0);
+                    @Override
+                    public Thread newThread(@NonNull Runnable task) {
+                        return new Thread(task, "server-thread-" + threadIdGenerator.incrementAndGet());
+                    }
+                });
+
+        var factory = new NettyReactiveWebServerFactory();
+        factory.addServerCustomizers(builder -> builder.runOn(eventLoopGroup));
+
+        return factory;
+    }
+
+    @Bean
+    public Scheduler workerPool() {
+        return Schedulers.newParallel("worker-thread", THREAD_POOL_SIZE);
+    }
+}
