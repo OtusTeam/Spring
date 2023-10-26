ed2cc244f0c6d4d7294e2ebb7bb1df351b3c109c
stvort <stvort@mail.ru>
2023-10-26 23:03:05 +0400

    hw03-spring-boot-template updated

--- templates/hw03-spring-boot/src/main/java/ru/otus/hw/config/AppConfig.java
+++ templates/hw03-spring-boot/src/main/java/ru/otus/hw/config/AppConfig.java
@@ -1,17 +1,20 @@
 package ru.otus.hw.config;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Locale;
 import java.util.Map;
 
-@Data
+@Setter
 // Использовать @ConfigurationProperties.
 // Сейчас класс соответствует файлу настроек. Чтобы они сюда отобразились нужно только правильно разместить аннотации
 public class AppConfig implements TestConfig, TestFileNameProvider, LocaleConfig {
 
+    @Getter
     private int rightAnswersCountToPass;
 
+    @Getter
     private Locale locale;
 
     private Map<String, String> fileNameByLocaleTag;
