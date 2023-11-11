c11ea43eeb00bad605a2796b89955763f7d38888
stvort <stvort@mail.ru>
2023-11-11 11:46:05 +0400

    2023-09 spring-11-jpql added

---
+++ 2023-09/spring-11-jpql/jpql-class-work/jpql-solution-06/src/main/java/ru/otus/example/ormdemo/models/EMail.java
@@ -0,0 +1,26 @@
+package ru.otus.example.ormdemo.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "emails")
+public class EMail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+}
