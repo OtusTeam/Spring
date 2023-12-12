b71d48511d242741f2c38553f758a7fba44c4d37
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2023-12-12 19:58:26 +0300

    2023-11 spring-04-aop

---
+++ 2023-11/spring-04-aop/aop-classwork/solution/src/main/java/ru/otus/spring/Main.java
@@ -0,0 +1,25 @@
+package ru.otus.spring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import ru.otus.spring.domain.Person;
+import ru.otus.spring.service.PersonService;
+
+@EnableAspectJAutoProxy
+@Configuration
+@ComponentScan
+public class Main {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(Main.class);
+
+		PersonService service = context.getBean(PersonService.class);
+
+		Person ivan = service.getByName("Ivan");
+		System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
+	}
+}
