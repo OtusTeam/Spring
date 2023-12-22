959472980bc2d9becfada50c92315adb3506ef94
petrelevich <petrelevich@yandex.ru>
2023-12-22 22:10:40 +0300

    L-39 kafka

---
+++ 2023-07/spring-39-kafka/producer/src/main/java/ru/demo/service/StringValueSource.java
@@ -0,0 +1,30 @@
+package ru.demo.service;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.demo.model.StringValue;
+
+public class StringValueSource implements ValueSource {
+    private static final Logger log = LoggerFactory.getLogger(StringValueSource.class);
+    private final AtomicLong nextValue = new AtomicLong(1);
+    private final DataSender valueConsumer;
+
+    public StringValueSource(DataSender dataSender) {
+        this.valueConsumer = dataSender;
+    }
+
+    @Override
+    public void generate() {
+        var executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(() -> valueConsumer.send(makeValue()), 0, 1, TimeUnit.SECONDS);
+        log.info("generation started");
+    }
+
+    private StringValue makeValue() {
+        var id = nextValue.getAndIncrement();
+        return new StringValue(id, "stVal:" + id);
+    }
+}
