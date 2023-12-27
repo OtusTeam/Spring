1fd5724d770d46c77f0cc0b3ef5bae836118ba95
petrelevich <petrelevich@yandex.ru>
2023-12-27 10:27:47 +0300

    L40-kafka-webflux

---
+++ 2023-07/spring-40-kafka-webflux/source/src/main/java/com/datasrc/config/ApplConfig.java
@@ -0,0 +1,52 @@
+package com.datasrc.config;
+
+import io.netty.channel.nio.NioEventLoopGroup;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicLong;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+import reactor.util.annotation.NonNull;
+
+@Configuration
+public class ApplConfig {
+    private static final int THREAD_POOL_SIZE = 2;
+    private static final int BLOCKING_THREAD_POOL_SIZE = 2;
+
+    @Bean
+    public ReactiveWebServerFactory reactiveWebServerFactory() {
+        var eventLoopGroup = new NioEventLoopGroup(THREAD_POOL_SIZE,
+                new ThreadFactory() {
+                    private final AtomicLong threadIdGenerator = new AtomicLong(0);
+
+                    @Override
+                    public Thread newThread(@NonNull Runnable task) {
+                        return new Thread(task, "server-thread-" + threadIdGenerator.incrementAndGet());
+                    }
+                });
+
+        var factory = new NettyReactiveWebServerFactory();
+        factory.addServerCustomizers(builder -> builder.runOn(eventLoopGroup));
+
+        return factory;
+    }
+
+    @Bean("blockingExecutor")
+    public Executor blockingExecutor() {
+        var id = new AtomicLong(0);
+        return Executors.newFixedThreadPool(BLOCKING_THREAD_POOL_SIZE,
+                task -> new Thread(task, String.format("blocking-thread-%d", id.incrementAndGet())));
+    }
+
+    @Bean
+    public Scheduler timer() {
+        return Schedulers.newParallel("processor-thread", 2);
+    }
+}
