ef0332c936e06371d564cfc15196913410138ae0
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2024-06-01 10:45:07 +0300

    2024-01 spring-30-endpoints-flow-components

---
+++ 2024-01/spring-30-endpoints-flow-components/endpoints-flow-components-exercise/src/main/java/ru/otus/spring/integration/App.java
@@ -0,0 +1,17 @@
+package ru.otus.spring.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import ru.otus.spring.integration.services.OrderService;
+
+
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = SpringApplication.run(App.class, args);
+		OrderService orderService = ctx.getBean(OrderService.class);
+		orderService.startGenerateOrdersLoop();
+	}
+}
