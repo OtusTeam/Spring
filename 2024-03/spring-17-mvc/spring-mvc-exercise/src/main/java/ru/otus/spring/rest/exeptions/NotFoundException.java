f4d0563ca4bf6cb520fafc9e16507f38b59189fb
stvort <stvort@mail.ru>
2024-06-10 19:29:16 +0400

    2024-03 spring-17-mvc added

---
+++ 2024-03/spring-17-mvc/spring-mvc-exercise/src/main/java/ru/otus/spring/rest/exeptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package ru.otus.spring.rest.exeptions;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException() {
+    }
+}
---
+++ 2024-03/spring-17-mvc/spring-mvc-solution-3/src/main/java/ru/otus/spring/rest/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package ru.otus.spring.rest.exceptions;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException() {
+    }
+}
