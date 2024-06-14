ed0055dd72aae38f7019ba0fc9652f4f656d8c77
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2024-06-14 19:28:45 +0300

    2024-05 spring-04-aop

---
+++ 2024-05/spring-04-aop/aop-demo/aop-custom/src/main/java/ru/otus/demo/domain/Person.java
@@ -0,0 +1,28 @@
+package ru.otus.demo.domain;
+
+public class Person {
+
+	private final String name;
+	private final int age;
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person{" +
+				"name='" + name + '\'' +
+				", age=" + age +
+				'}';
+	}
+}
