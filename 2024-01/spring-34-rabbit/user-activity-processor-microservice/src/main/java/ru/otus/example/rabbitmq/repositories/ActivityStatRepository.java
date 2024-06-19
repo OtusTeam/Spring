5680287cbb38b99efa2c8be764e6c30db9a9b65f
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2024-06-19 19:57:38 +0300

    2024-01 spring-34 rabbit

---
+++ 2024-01/spring-34-rabbit/user-activity-processor-microservice/src/main/java/ru/otus/example/rabbitmq/repositories/ActivityStatRepository.java
@@ -0,0 +1,22 @@
+package ru.otus.example.rabbitmq.repositories;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import ru.otus.example.useractivitymodels.ActivityStatElem;
+
+@Transactional
+public interface ActivityStatRepository extends JpaRepository<ActivityStatElem, Long> {
+
+	@Transactional(readOnly = true)
+	@Query("select new ru.otus.example.useractivitymodels.ActivityStatElem(u.name, t.name, count(a)) " +
+			"from UserActivity a left join a.appUser u left join a.type t " +
+			"group by u.name, t.name " +
+			"order by count(a) desc, u.name, t.name")
+	List<ActivityStatElem> calcActivityStat();
+
+}
