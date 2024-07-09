9a9fa87d449167595e4994d7997d16357c06e5b4
petrelevich <petrelevich@yandex.ru>
2024-07-09 21:05:09 +0300

    L40-kafka-webflux

---
+++ 2024-01/spring-40-kafka-webflux/common/src/main/java/com/datasrc/config/ConsumerException.java
@@ -0,0 +1,7 @@
+package com.datasrc.config;
+
+public class ConsumerException extends RuntimeException {
+    public ConsumerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
