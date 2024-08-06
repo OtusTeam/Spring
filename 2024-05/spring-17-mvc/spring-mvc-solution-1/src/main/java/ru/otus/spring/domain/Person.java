d071d3659a2ba524bf64f4b4591aab95cd4fb774
stvort <stvort@mail.ru>
2024-08-06 19:53:05 +0400

    2024-05 spring-17-mvc added

---
+++ 2024-05/spring-17-mvc/spring-mvc-solution-1/src/main/java/ru/otus/spring/domain/Person.java
@@ -0,0 +1,40 @@
+package ru.otus.spring.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    private long id;
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
