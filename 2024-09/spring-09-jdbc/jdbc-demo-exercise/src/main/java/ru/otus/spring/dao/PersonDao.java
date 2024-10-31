6e6b0fdce62386b38e5ac1924a35cd6c3a6a1f9a
stvort <stvort@mail.ru>
2024-10-31 20:49:48 +0400

    2024-09 spring-09-jdbc added

---
+++ 2024-09/spring-09-jdbc/jdbc-demo-exercise/src/main/java/ru/otus/spring/dao/PersonDao.java
@@ -0,0 +1,17 @@
+package ru.otus.spring.dao;
+
+import ru.otus.spring.domain.Person;
+
+import java.util.List;
+
+public interface PersonDao {
+    int count();
+
+    void insert(Person person);
+
+    Person getById(long id);
+
+    List<Person> getAll();
+
+    void deleteById(long id);
+}
