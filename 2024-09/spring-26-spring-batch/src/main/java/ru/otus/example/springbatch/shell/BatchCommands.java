d9cc2638f86689a1d23274ec176ac69a57220241
stvort <stvort@mail.ru>
2024-12-19 19:57:53 +0400

    2024-09 spring-27-spring-batch added

---
+++ 2024-09/spring-26-spring-batch/src/main/java/ru/otus/example/springbatch/shell/BatchCommands.java
@@ -0,0 +1,61 @@
+package ru.otus.example.springbatch.shell;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import ru.otus.example.springbatch.config.AppProps;
+
+import java.util.Properties;
+
+import static ru.otus.example.springbatch.config.JobConfig.IMPORT_USER_JOB_NAME;
+import static ru.otus.example.springbatch.config.JobConfig.INPUT_FILE_NAME;
+import static ru.otus.example.springbatch.config.JobConfig.OUTPUT_FILE_NAME;
+
+@RequiredArgsConstructor
+@ShellComponent
+public class BatchCommands {
+
+    private final AppProps appProps;
+    private final Job importUserJob;
+
+    private final JobLauncher jobLauncher;
+    private final JobOperator jobOperator;
+    private final JobExplorer jobExplorer;
+
+    //http://localhost:8080/h2-console/
+
+
+    @SuppressWarnings("unused")
+    @ShellMethod(value = "startMigrationJobWithJobLauncher", key = "sm-jl")
+    public void startMigrationJobWithJobLauncher() throws Exception {
+        JobExecution execution = jobLauncher.run(importUserJob, new JobParametersBuilder()
+                .addString(INPUT_FILE_NAME, appProps.getInputFile())
+                .addString(OUTPUT_FILE_NAME, appProps.getOutputFile())
+                .toJobParameters());
+        System.out.println(execution);
+    }
+
+    @SuppressWarnings("unused")
+    @ShellMethod(value = "startMigrationJobWithJobOperator", key = "sm-jo")
+    public void startMigrationJobWithJobOperator() throws Exception {
+        Properties properties = new Properties();
+        properties.put(INPUT_FILE_NAME, appProps.getInputFile());
+        properties.put(OUTPUT_FILE_NAME, appProps.getOutputFile());
+
+        Long executionId = jobOperator.start(IMPORT_USER_JOB_NAME, properties);
+        System.out.println(jobOperator.getSummary(executionId));
+    }
+
+    @SuppressWarnings("unused")
+    @ShellMethod(value = "showInfo", key = "i")
+    public void showInfo() {
+        System.out.println(jobExplorer.getJobNames());
+        System.out.println(jobExplorer.getLastJobInstance(IMPORT_USER_JOB_NAME));
+    }
+}
