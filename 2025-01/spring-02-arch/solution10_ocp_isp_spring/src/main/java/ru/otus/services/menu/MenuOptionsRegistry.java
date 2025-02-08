caa202461a5198ee291cab5e046b634c2e6575a7
stvort <stvort@mail.ru>
2025-02-08 11:26:02 +0400

    2025-01 spring-02-arch added

---
+++ 2025-01/spring-02-arch/solution10_ocp_isp_spring/src/main/java/ru/otus/services/menu/MenuOptionsRegistry.java
@@ -0,0 +1,10 @@
+package ru.otus.services.menu;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MenuOptionsRegistry {
+    List<MenuOption> getAvailableMenuOptions();
+
+    Optional<MenuOption> getMenuOptionById(int id);
+}
