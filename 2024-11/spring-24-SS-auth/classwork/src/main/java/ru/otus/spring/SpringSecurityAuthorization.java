fc5dad76bf4385743ddc3df1ceda56255b48ac0a
Uladzimir <vladimir)magerov@epam.com>
2025-03-04 18:27:47 +0300

    2024-11 spring-24-auth

---
+++ 2024-11/spring-24-SS-auth/classwork/src/main/java/ru/otus/spring/SpringSecurityAuthorization.java
@@ -0,0 +1,13 @@
+package ru.otus.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthorization {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAuthorization.class);
+    }
+
+}
