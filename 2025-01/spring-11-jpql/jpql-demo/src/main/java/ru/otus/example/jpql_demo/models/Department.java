cb33e4f12d009a965df89e7445ba64773c373f31
stvort <stvort@mail.ru>
2025-03-15 02:43:57 +0400

    2025-01 spring-11-jpql added

---
+++ 2025-01/spring-11-jpql/jpql-demo/src/main/java/ru/otus/example/jpql_demo/models/Department.java
@@ -0,0 +1,25 @@
+package ru.otus.example.jpql_demo.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "departments")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+}
