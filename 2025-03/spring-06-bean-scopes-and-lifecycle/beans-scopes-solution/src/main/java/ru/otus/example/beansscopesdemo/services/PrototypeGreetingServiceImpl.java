f3e6975bb4b3bee2aaa8995e2059e6cde02b2d2f
stvort <stvort@mail.ru>
2025-04-24 20:40:26 +0400

    2025-03 spring-06-bean-scopes-and-lifecycle added

---
+++ 2025-03/spring-06-bean-scopes-and-lifecycle/beans-scopes-solution/src/main/java/ru/otus/example/beansscopesdemo/services/PrototypeGreetingServiceImpl.java
@@ -0,0 +1,10 @@
+package ru.otus.example.beansscopesdemo.services;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Service;
+
+@Scope(scopeName = "prototype")
+@Service("prototypeGreetingService")
+public class PrototypeGreetingServiceImpl extends AbstractGreetingServiceImpl {
+}
