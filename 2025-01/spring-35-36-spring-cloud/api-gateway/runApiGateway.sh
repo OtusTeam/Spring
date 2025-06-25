1a7aada5725cd456ae9f4f7436d022823bcd4b1a
petrelevich <petrelevich@yandex.ru>
2025-06-25 07:33:21 +0300

    spring cloud

--- 2025-01/spring-35-36-spring-cloud/api-gateway/runApiGateway.sh
+++ 2025-01/spring-35-36-spring-cloud/api-gateway/runApiGateway.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-../gradlew :config-server:build
+../gradlew :api-gateway:build
 
 docker load --input build/jib-image.tar
 
-docker stop config-server
+docker stop api-gateway
 
-docker run --rm -d --name config-server \
---memory=1024m \
+docker run --rm -d --name api-gateway \
+--memory=512m \
 --cpus 1 \
 --network="host" \
 -v $HOME/.ssh:/root/.ssh \
 -e JAVA_TOOL_OPTIONS="-XX:InitialRAMPercentage=80 -XX:MaxRAMPercentage=80" \
-localrun/config-server:latest
+localrun/api-gateway:latest
 
--- 2025-01/spring-35-36-spring-cloud/eureka-server/runEurekaServer.sh
+++ 2025-01/spring-35-36-spring-cloud/eureka-server/runEurekaServer.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-../gradlew :config-server:build
+../gradlew :eureka-server:build
 
 docker load --input build/jib-image.tar
 
-docker stop config-server
+docker stop eureka-server
 
-docker run --rm -d --name config-server \
---memory=1024m \
+docker run --rm -d --name eureka-server \
+--memory=512m \
 --cpus 1 \
 --network="host" \
 -v $HOME/.ssh:/root/.ssh \
 -e JAVA_TOOL_OPTIONS="-XX:InitialRAMPercentage=80 -XX:MaxRAMPercentage=80" \
-localrun/config-server:latest
+localrun/eureka-server:latest
 
