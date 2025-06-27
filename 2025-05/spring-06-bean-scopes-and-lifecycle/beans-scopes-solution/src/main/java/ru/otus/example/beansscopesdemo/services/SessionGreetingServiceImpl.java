bfcb618c8016c8edc6c4bed8448294f3fb1a50e8
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2025-06-27 19:40:36 +0300

    2025-05 spring-06-bean-scopes-and-lifecycle

---
+++ 2025-05/spring-06-bean-scopes-and-lifecycle/beans-scopes-solution/src/main/java/ru/otus/example/beansscopesdemo/services/SessionGreetingServiceImpl.java
@@ -0,0 +1,10 @@
+package ru.otus.example.beansscopesdemo.services;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Service;
+
+@Scope(value = "session", proxyMode = ScopedProxyMode.INTERFACES)
+@Service("sessionGreetingService")
+public class SessionGreetingServiceImpl extends AbstractGreetingServiceImpl {
+}
