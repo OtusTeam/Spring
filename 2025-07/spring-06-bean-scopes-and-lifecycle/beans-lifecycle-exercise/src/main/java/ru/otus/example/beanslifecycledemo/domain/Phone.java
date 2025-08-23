6a43d20daa29c16ac573143549b6abff14cbbcf6
stvort <stvort@mail.ru>
2025-08-23 12:01:33 +0400

    2025-07 spring-06-bean-scopes-and-lifecycle

---
+++ 2025-07/spring-06-bean-scopes-and-lifecycle/beans-lifecycle-exercise/src/main/java/ru/otus/example/beanslifecycledemo/domain/Phone.java
@@ -0,0 +1,19 @@
+package ru.otus.example.beanslifecycledemo.domain;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@ConditionalOnProperty(name = "lifecycle.print.enabled", havingValue = "false")
+@RequiredArgsConstructor
+public class Phone {
+	private final String greeting = "Погнали к родителям";
+
+	private final PhoneNumber favoriteNumber;
+
+	public void callFavoriteNumber() {
+		System.out.println(favoriteNumber.getOwnerName() + " " + greeting);
+	}
+}
