a874a8dd15d4a5a3f7ac8b6108e58a7c89302536
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2025-11-15 10:34:11 +0300

    2025-07 spring-30-endpoints-flow-components

---
+++ 2025-07/spring-30-endpoints-flow-components/endpoints-flow-components-exercise/src/main/java/ru/otus/spring/integration/App.java
@@ -0,0 +1,12 @@
+package ru.otus.spring.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
