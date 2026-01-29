1e8923dbb12235c68bd2fe995209677dbacbdeda
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2026-01-29 19:24:55 +0300

    2025-09 spring-32 http-client

---
+++ 2025-09/spring-32-http-client/soap-client/src/main/java/hello/CountryClient.java
@@ -0,0 +1,24 @@
+package hello;
+
+import hello.wsdl.GetCountryRequest;
+import hello.wsdl.GetCountryResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+
+@Slf4j
+public class CountryClient extends WebServiceGatewaySupport {
+
+    public GetCountryResponse getCountry(String country) {
+
+        GetCountryRequest request = new GetCountryRequest();
+        request.setName(country);
+
+        log.info("Requesting location for {}", country);
+
+        GetCountryResponse response = (GetCountryResponse) getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:8080/ws", request);
+
+        return response;
+    }
+
+}
