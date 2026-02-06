b8d7ab52f075b98f514c76fb6898691c0ba51de1
stvort <stvort@mail.ru>
2026-02-06 20:12:03 +0400

    2025-11 spring-16-view

---
+++ 2025-11/spring-16-view/spring-mvc-view-solution1/src/main/java/ru/otus/spring/Main.java
@@ -0,0 +1,13 @@
+package ru.otus.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//http://localhost:8080/edit?id=1
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+}
