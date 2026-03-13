374625782a7f0cbc0f82b6b1477513997d73e7bd
stvort <stvort@mail.ru>
2026-03-13 19:59:48 +0400

    2025-11 spring-24-spring-batch added

---
+++ 2025-11/spring-24-spring-batch/src/main/java/ru/otus/example/springbatch/model/Person.java
@@ -0,0 +1,13 @@
+package ru.otus.example.springbatch.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Person {
+    private String name;
+    private int age;
+}
