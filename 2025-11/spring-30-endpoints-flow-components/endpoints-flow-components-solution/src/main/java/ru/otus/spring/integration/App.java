fd536d2d8d2a5165df6bd1f1a0f7dfe6ab26337a
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2026-03-24 19:35:34 +0300

    2025-11 spring-30-endpoints-flow-components

---
+++ 2025-11/spring-30-endpoints-flow-components/endpoints-flow-components-solution/src/main/java/ru/otus/spring/integration/App.java
@@ -0,0 +1,14 @@
+package ru.otus.spring.integration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
