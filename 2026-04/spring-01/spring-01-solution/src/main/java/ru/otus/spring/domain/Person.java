abc139afa5c90dc4e94161f7dcdff3db33f70307
stvort <stvort@mail.ru>
2026-05-07 19:40:08 +0400

    2026-04 spring 01

---
+++ 2026-04/spring-01/spring-01-solution/src/main/java/ru/otus/spring/domain/Person.java
@@ -0,0 +1,29 @@
+package ru.otus.spring.domain;
+
+public class Person {
+
+    private final String name;
+
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
