0c5faffbac1a288222c976208b92e1801ecadba8
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2026-05-20 19:26:22 +0300

    2026-01 spring-29-integrations-channels

---
+++ 2026-01/spring-29-integrations-channels/integrations-channels-exercise/src/main/java/ru/otus/spring/integration/IntegrationConfig.java
@@ -0,0 +1,23 @@
+package ru.otus.spring.integration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.dsl.DirectChannelSpec;
+import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.messaging.PollableChannel;
+
+@Configuration
+public class IntegrationConfig {
+
+    @Bean
+    public PollableChannel queueChannel() {
+        return new QueueChannel(100);
+    }
+
+    @Bean
+    public DirectChannelSpec subscribableDirectChannel() {
+        return MessageChannels.direct("subscribableDirectChannel");
+    }
+
+}
