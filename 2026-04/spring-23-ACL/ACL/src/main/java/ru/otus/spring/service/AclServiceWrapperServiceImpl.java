91788b70fe967ae34e25cce1528f9ca272876650
VladimirMagerov <vladimirfloyd@yandex.ru>
2026-07-30 17:28:42 +0300

    2026-04 spring-SS-auth-acl

---
+++ 2026-04/spring-23-ACL/ACL/src/main/java/ru/otus/spring/service/AclServiceWrapperServiceImpl.java
@@ -0,0 +1,34 @@
+package ru.otus.spring.service;
+
+import org.springframework.security.acls.domain.BasePermission;
+import org.springframework.security.acls.domain.GrantedAuthoritySid;
+import org.springframework.security.acls.domain.ObjectIdentityImpl;
+import org.springframework.security.acls.domain.PrincipalSid;
+import org.springframework.security.acls.model.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AclServiceWrapperServiceImpl implements AclServiceWrapperService {
+
+    private final MutableAclService mutableAclService;
+
+    public AclServiceWrapperServiceImpl(MutableAclService mutableAclService) {
+        this.mutableAclService = mutableAclService;
+    }
+
+    @Override
+    public void createPermission(Object object, Permission permission) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        final Sid owner = new PrincipalSid(authentication);
+        ObjectIdentity oid = new ObjectIdentityImpl(object);
+
+        final Sid admin = new GrantedAuthoritySid("ROLE_EDITOR");
+
+        MutableAcl acl = mutableAclService.createAcl(oid);
+        acl.insertAce(acl.getEntries().size(), permission, owner, true);
+        acl.insertAce(acl.getEntries().size(), permission, admin, true);
+        mutableAclService.updateAcl(acl);
+    }
+}
---
+++ 2026-04/spring-23-ACL/oauth/src/main/java/ru/otus/spring/sso/controller/IndexController.java
@@ -0,0 +1,13 @@
+package ru.otus.spring.sso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String indexPage() {
+        return "index";
+    }
+}
