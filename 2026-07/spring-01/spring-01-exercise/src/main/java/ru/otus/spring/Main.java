fa0ff81aa0dd9e8d438f3713a97f677b1b2a2065
Dabramov <dabramov@naumen.ru>
2026-08-21 21:33:45 +0500

    spring-01

---
+++ 2026-07/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/Main.java
@@ -0,0 +1,17 @@
+package ru.otus.spring;
+
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.otus.spring.domain.Person;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO: создайте здесь класс контекста
+
+        // TODO: Получите Person Service
+
+        // Получите Person "Ivan"
+        Person ivan = null;
+        System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
+    }
+}
