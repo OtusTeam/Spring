7a86b644eade87e88d4aaf43e68810b0b7df01b3
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2026-09-15 20:08:27 +0300

    2026-07 spring-9-jdbc

---
+++ 2026-07/spring-09-jdbc/jdbc-demo-solution-5/src/main/java/ru/otus/spring/dao/PersonDao.java
@@ -0,0 +1,17 @@
+package ru.otus.spring.dao;
+
+import java.util.List;
+
+import ru.otus.spring.domain.Person;
+
+public interface PersonDao {
+	int count();
+
+	void insert(Person person);
+
+	Person getById(long id);
+
+	List<Person> getAll();
+
+	void deleteById(long id);
+}
